904630e65573dec6f1f57bef8d84102bf31899d6
Izabelle Wilding <izabellewilding@gmail.com>
2022-05-17 11:16:43 +0100

    Sort by launch date

--- src/components/LaunchList/LaunchList.jsx
+++ src/components/LaunchList/LaunchList.jsx
@@ -4,7 +4,6 @@ import { LaunchItem } from "../LaunchItem";
 export const LaunchList = ({ items, filter, sort }) => {
   const [filteredItems, setFilteredItems] = useState([...items]);
 
-  // let filteredItems = [...items];
   useEffect(() => {
     if (filter !== "") {
       setFilteredItems(items.filter((item) => item.launch_year === filter));
@@ -15,8 +14,8 @@ export const LaunchList = ({ items, filter, sort }) => {
 
   //Bug in the sorting function below
   const launches = filteredItems.sort((a, b) => {
-    const x = a.launch_year;
-    const y = b.launch_year;
+    const x = a.launch_date_unix;
+    const y = b.launch_date_unix;
     return sort ? y - x : x - y;
   });
 
