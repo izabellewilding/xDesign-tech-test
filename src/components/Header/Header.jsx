ab90f0f929ced1d58b332575bf9be9491ebfcfb9
Izabelle Wilding <izabellewilding@gmail.com>
2022-05-17 13:44:33 +0100

    Add logo to Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import LABEL from "../../constants/Labels";
 import { Button } from "../../components/Button";
 import { useLaunchContext } from "../../contexts/LaunchContext/LaunchContext";
+import CONSTANTS from "../../constants/Config";
 
 export const Header = () => {
   const { filter } = useLaunchContext();
   return (
     <header className="app__header">
       <div className="app__logo">
-        {/* missing src  */}
-        <img className="app__logo-image" alt="" />
+        <img
+          className="app__logo-image"
+          alt="spacex-logo"
+          src={CONSTANTS.LOGO}
+        />
         <span className="app__logo-txt">{LABEL.LAUNCHES}</span>
       </div>
       <Button
