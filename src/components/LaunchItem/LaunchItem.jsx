35500e465b76ee5a511e3fe2454f0951dacc9777
Izabelle Wilding <izabellewilding@gmail.com>
2022-05-17 13:25:11 +0100

    Style list items for desktop

--- src/components/LaunchItem/LaunchItem.jsx
+++ src/components/LaunchItem/LaunchItem.jsx
@@ -2,20 +2,19 @@ import React from "react";
 import moment from "moment";
 
 export const LaunchItem = ({ item, index }) => {
-  //missing rocket name
   const { flight_number, mission_name, launch_date_utc, rocket } = item;
 
   return (
-    <li key={index}>
-      <div>
-        <span>{`#${flight_number}`}</span>
+    <li key={index} className="launch-item">
+      <div className="launch-item-primary">
+        <span className="launch-item-number">{`#${flight_number}`}</span>
         <span>{`${mission_name}`}</span>
       </div>
-      <div>
-        <span>
-          <span> {moment(launch_date_utc).format("MMM Do YY")}</span>
-          <span>{rocket.rocket_name}</span>
+      <div className="launch-item-secondary">
+        <span className="launch-item-date">
+          {moment(launch_date_utc).format("MMM Do YY")}
         </span>
+        <span className="launch-item-rocket-name">{rocket.rocket_name}</span>
       </div>
     </li>
   );
